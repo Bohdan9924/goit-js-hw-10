15b064363d57193a3b019e47778b57f7782f80b0
Bohdan9924 <bohdanminiailo22@gmail.com>
2023-06-18 21:00:18 +0200

    complete task 1

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,48 +1,3 @@
-// const apiAllCat = 'https://api.thecatapi.com/v1/breeds';
-// const apiSearchCat = 'https://api.thecatapi.com/v1/images/search';
-
-// const API_KEY = 'live_TEVQVH8kIwhaeRhDOSyA8WnwNlykjxrHb3yygHQV8UxCfcTR5OszaMDhm7NejJnZ';
-
-// export function fetchBreeds() {
-//     return new Promise((resolve, reject) => {
-//         fetch(apiAllCat, {
-//             headers: {
-//                 'x-api-key': API_KEY,
-//             }
-//         }).then(response => {
-//                 if (!response.ok) {
-//                     throw new Error('HTTP error' + response.status);
-//                 }
-//                 return response.json();
-//             })
-//             .then(data => {
-//                 resolve(data.breeds);
-//             }).catch(error => {
-//         reject(error);
-//       });
-// });
-// };
-
-// export function fetchCatByBreed(breedId) {
-//     return new Promise((resolve, reject) => {
-//         fetch(apiSearchCat, {
-//             headers: {
-//                 'x-api-key': API_KEY,
-//             }
-//         }).then(response => {
-//                 if (!response.ok) {
-//                     throw new Error('HTTP error' + response.status);
-//                 }
-//                 return response.json();
-//             })
-//             .then(data => {
-//                 resolve(data[0]);
-//             }).catch(error => {
-//         reject(error);
-//       });
-//     })
-// }
-
 const apiFull = 'https://api.thecatapi.com/v1/breeds';
 const apiSearch = 'https://api.thecatapi.com/v1/images/search';
 const API_KEY =
@@ -56,12 +11,12 @@ const API_KEY =
   })
     .then(response => {
       if (!response.ok) {
-        throw new Error(response.status);
+        throw new Error('HTTP error ' + response.status);
       }
       return response.json();
     })
     .catch(error => {
-      console.warn(error);
+      throw error;
     });
 }
 
@@ -73,11 +28,11 @@ export function fetchCatByBreed(breedId) {
   })
     .then(response => {
       if (!response.ok) {
-        throw new Error(response.status);
+        throw new Error('HTTP error ' + response.status);
       }
       return response.json();
     })
     .catch(error => {
-      console.warn(error);
+      throw error;
     });
 }
