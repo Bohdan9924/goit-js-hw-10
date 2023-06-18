f38acaa976942425bafa80e2a7c6d94dfd423bbb
Bohdan9924 <bohdanminiailo22@gmail.com>
2023-06-18 16:45:30 +0200

    in processing task 1

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,44 +1,83 @@
-const apiAllCat = 'https://api.thecatapi.com/v1/breeds';
-const apiSearchCat = 'https://api.thecatapi.com/v1/images/search';
+// const apiAllCat = 'https://api.thecatapi.com/v1/breeds';
+// const apiSearchCat = 'https://api.thecatapi.com/v1/images/search';
 
-const API_KEY = 'live_TEVQVH8kIwhaeRhDOSyA8WnwNlykjxrHb3yygHQV8UxCfcTR5OszaMDhm7NejJnZ';
+// const API_KEY = 'live_TEVQVH8kIwhaeRhDOSyA8WnwNlykjxrHb3yygHQV8UxCfcTR5OszaMDhm7NejJnZ';
 
-export function fetchBreeds() {
-    return new Promise((resolve, reject) => {
-        fetch(apiAllCat, {
-            headers: {
-                'x-api-key': API_KEY,
-            }
-        }).then(response => {
-                if (!response.ok) {
-                    throw new Error('HTTP error' + response.status);
-                }
-                return response.json();
-            })
-            .then(data => {
-                resolve(data.breeds);
-            }).catch(error => {
-        reject(error);
-      });
-});
-};
+// export function fetchBreeds() {
+//     return new Promise((resolve, reject) => {
+//         fetch(apiAllCat, {
+//             headers: {
+//                 'x-api-key': API_KEY,
+//             }
+//         }).then(response => {
+//                 if (!response.ok) {
+//                     throw new Error('HTTP error' + response.status);
+//                 }
+//                 return response.json();
+//             })
+//             .then(data => {
+//                 resolve(data.breeds);
+//             }).catch(error => {
+//         reject(error);
+//       });
+// });
+// };
+
+// export function fetchCatByBreed(breedId) {
+//     return new Promise((resolve, reject) => {
+//         fetch(apiSearchCat, {
+//             headers: {
+//                 'x-api-key': API_KEY,
+//             }
+//         }).then(response => {
+//                 if (!response.ok) {
+//                     throw new Error('HTTP error' + response.status);
+//                 }
+//                 return response.json();
+//             })
+//             .then(data => {
+//                 resolve(data[0]);
+//             }).catch(error => {
+//         reject(error);
+//       });
+//     })
+// }
+
+const apiFull = 'https://api.thecatapi.com/v1/breeds';
+const apiSearch = 'https://api.thecatapi.com/v1/images/search';
+const API_KEY =
+  'live_J1c3ReKXpbII84HmPX4RyPejOXkcqD0fyEejmLwc7SlwdX9GizKFc0O7vazMR0yY';
+
+  export function fetchBreeds() {
+  return fetch(apiFull, {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.warn(error);
+    });
+}
 
 export function fetchCatByBreed(breedId) {
-    return new Promise((resolve, reject) => {
-        fetch(apiSearchCat, {
-            headers: {
-                'x-api-key': API_KEY,
-            }
-        }).then(response => {
-                if (!response.ok) {
-                    throw new Error('HTTP error' + response.status);
-                }
-                return response.json();
-            })
-            .then(data => {
-                resolve(data[0]);
-            }).catch(error => {
-        reject(error);
-      });
+  return fetch(`${apiSearch}?breed_ids=${breedId}`, {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
     })
+    .catch(error => {
+      console.warn(error);
+    });
 }
--- src/index.js
+++ src/index.js
@@ -71,3 +71,75 @@ fetchBreeds()
 
 
 
+// import { fetchBreeds } from './cat-api';
+// import { fetchCatByBreed } from './cat-api';
+
+// import Notiflix from 'notiflix'
+
+// const selectEl = document.querySelector('.breed-select');
+// const catInfoEl = document.querySelector('.cat-info');
+// const loaderEl = document.querySelector('.loader');
+// const errorEl = document.querySelector('.error');
+
+// function showLoad() {
+//   loaderEl.classList.add('active');
+// }
+
+// function hideLoad() {
+//   loaderEl.classList.remove('active');
+// }
+
+// function showError() {
+//   errorEl.classList.add('active');
+// }
+
+// function hideError() {
+//   errorEl.classList.remove('active');
+// }
+
+// fetchBreeds()
+//   .then(data => {
+//     let optionMarkup = '';
+//     data.forEach(element => {
+//       optionMarkup += `<option value="${element.id}">${element.name}</option>`;
+//     });
+//     selectEl.insertAdjacentHTML('afterbegin', optionMarkup);
+//   })
+//   .catch(error => {
+//     showError();
+//     Notiflix.Notify.warning(error);
+//   })
+//   .finally(() => {
+//     hideLoad();
+//   });
+
+// selectEl.addEventListener('change', event => {
+//   hideError();
+
+//   catInfoEl.innerHTML = '';
+//   const selectedCat = event.target.value;
+//   let catInfoMarkup = '';
+
+//   showLoad();
+
+//   fetchCatByBreed(selectedCat)
+//     .then(data => {
+//       data.forEach(element => {
+//         catInfoMarkup += `<img class="cat-img" src="${element.url}" alt="${element.breeds[0].name}">
+//       <div>
+//       <h2>${element.breeds[0].name}</h2>
+//       <p>${element.breeds[0].description}</p>
+//       <p><span class="cat-temperament">Temperament: </span>${element.breeds[0].temperament}</p>
+// </div>
+// `;
+//       });
+//       catInfoEl.insertAdjacentHTML('afterbegin', catInfoMarkup);
+//     })
+//     .catch(error => {
+//       showError();
+//       Notiflix.Notify.warning(error);
+//     })
+//     .finally(() => {
+//       hideLoad();
+//     });
+// });
